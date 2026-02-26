eecc31ff2fe39e2851819276b0e75b5a69f08d0c
sh11o <sshh03ou@gmail.com>
2026-02-26 22:30:42 +0900

    ToDoappの作成

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,31 +3,50 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState('')
+  const [time, setTime] = useState('')
+  const [contents, setContent] = useState([])
+  const [error, setError] = useState('')
+
+  const onChangeText = (e) => {
+    setText(e.target.value)
+  }
+
+  const onChangeTime = (e) => {
+    setTime(e.target.value)
+  }
+
+  const onClickButton = () => {
+    if(text === '' || time === ''){
+      setError('入力されていない項目があります')
+      return
+    }
+
+    setContent((prevContent) => {
+      return [...prevContent, { text, time }]
+    })
+    setText('')
+    setTime('')
+    setError('')
+  }
+
+  const sumTime = contents.reduce((sum, content) => sum + parseInt(content.time), 0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>学習内容 <input type='text' onChange={onChangeText} value={text} /> </p>
+        <p>学習時間 <input type='text' onChange={onChangeTime} value={time} /> 時間(h)</p>
+        <p>入力されている学習内容 : {text}</p>
+        <p>入力されている学習時間 : {time}</p>
+        <button onClick={onClickButton}>登録</button>
+        {error && <p>{error}</p>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <p>合計時間 : {sumTime} 時間</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
   server: {
     host: true, // true に設定すると、LAN やパブリックアドレスを含むすべてのアドレスをリッスン
     port: 5173, // 開発サーバーが使用するポート番号
+    watch: {
+      usePolling: true, // これが重要！ファイルを定期的にチェックするモード
+    },
   },
 })
